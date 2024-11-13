c5d2b3786415bacfee93bb83be7ee90c208dfa17
Chaima Ben Fredj <chaima.benfredj@studenti.unimi.it>
2024-11-13 23:05:18 +0100

    Loading data  with pandas

--- Project Code.py
+++ Project Code.py
@@ -1,1 +1,15 @@
-project code will be shown here
+# Project code will be shown here
+
+
+
+# Loading the datasets
+import pandas as pd
+
+# Loading the relevant columns from each dataset
+movies_df = pd.read_csv('Dataset/movie.tsv', sep='\t', usecols=['tconst', 'primaryTitle', 'genres'])
+ratings_df = pd.read_csv('Dataset/ratings.tsv', sep='\t', usecols=['tconst', 'averageRating', 'numVotes'])
+crew_df = pd.read_csv('Dataset/crew.tsv', sep='\t', usecols=['tconst', 'nconst', 'category'])
+person_df = pd.read_csv('Dataset/person.tsv', sep='\t', usecols=['nconst', 'primaryName', 'birthYear','deathYear','primaryProfession'])
+
+# You can now work with the datasets and perform analysis or merge them as needed.
+print(ratings_df.head(5))
